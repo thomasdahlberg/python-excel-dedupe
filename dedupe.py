a84c5856908a26c995857a0d1c8f3a1772a469a0
Thomas Dahlberg <thomas.dahlberg8@gmail.com>
2020-08-06 09:31:59 -0400

    creates header values

--- dedupe.py
+++ dedupe.py
@@ -1,1 +1,17 @@
+# Sort and Dedupe Program
+
 import openpyxl
+import pprint
+import os
+
+print('Opening workbook')
+
+os.chdir('../')
+wb = openpyxl.load_workbook('example.xlsx')
+sheet = wb['sheet1']
+header_values = []
+
+for i in range(1, sheet.max_column):
+    header_values.append(sheet.cell(row=2, column=i).value)
+
+print('Headers: ', header_values)
