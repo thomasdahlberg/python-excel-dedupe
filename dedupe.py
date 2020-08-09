15fd1866c6f2355ca1c7660820a635daf43ab210
Thomas Dahlberg <thomas.dahlberg8@gmail.com>
2020-08-09 16:24:35 -0400

    dedupes based on single criteria

--- dedupe.py
+++ dedupe.py
@@ -25,21 +25,53 @@ for key in row_keys:
         row_dict[header_values[i-1]] = sheet.cell(row=key, column=i).value
     document.append(row_dict)
 
+
+deduped_document = []
+unique_values = []
+
+# for sub in document:
+#     unique_values.append(sub[unique_key])
+# unique_values = list(set(unique_values))
+
+# print('Unique IDs: ', str(unique_values))
+# print('# of IDs: ', len(unique_values))
+
+# deduped_document = [d for d in document if d['ID'] in unique_values]
+
+for d in document:
+    if d['ID'] not in unique_values:
+        unique_values.append(d['ID'])
+        deduped_document.append(d)
+
+
+print('DEDUPED?: ', deduped_document)
+# print('Original length: ', len(document))
+print('Deduped length: ', len(deduped_document))
+
+
 def sort_by(e):
     return e['Admit Term']
 
-document.sort(key=sort_by)
+# document.sort(key=sort_by)
 
-print('Document: ', document)
+# print('Document: ', document)
 
 # wb.remove(wb['Sorted'])
 
-wb.create_sheet(index=1, title='Sorted')
-sorted_sheet = wb['Sorted']
-sorted_sheet.append(header_values)
+# wb.create_sheet(index=1, title='Sorted')
+# sorted_sheet = wb['Sorted']
+# sorted_sheet.append(header_values)
 
-for i in range(1, len(document)):
-    sorted_sheet.append(list(document[i].values()))
+# for i in range(1, len(document)):
+#     sorted_sheet.append(list(document[i].values()))
+
+
+wb.create_sheet(index=1, title='Deduped')
+deduped_sheet = wb['Deduped']
+deduped_sheet.append(header_values)
+
+for i in range(1, len(deduped_document)):
+    deduped_sheet.append(list(deduped_document[i].values()))
 
 wb.save(filename='example.xlsx')
 
