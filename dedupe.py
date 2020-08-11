5f2682a45ecf4fb9fb24675aa0eaa9283d065d53
Thomas Dahlberg <thomas.dahlberg8@gmail.com>
2020-08-10 21:23:55 -0400

    adds sort CLI arg and sort function

--- dedupe.py
+++ dedupe.py
@@ -1,7 +1,5 @@
-# Sort and Dedupe Program
 
 import openpyxl
-import pprint
 import os
 import sys
 
@@ -9,6 +7,7 @@ print('Opening workbook')
 
 file_arg = sys.argv[1]
 dedupe_arg = sys.argv[2]
+sort_arg = sys.argv[3]
 
 
 os.chdir('./workdocs')
@@ -17,6 +16,11 @@ sheet = wb['sheet1']
 header_values = []
 row_keys = range(3,sheet.max_row)
 document = []
+deduped_document = []
+unique_values = []
+
+def sort_by(e):
+    return e[sort_col]
 
 for i in range(1, sheet.max_column + 1):
     header_values.append(sheet.cell(row=2, column=i).value)
@@ -25,6 +29,9 @@ print('Headers: ', header_values)
 print('Row Keys: ', row_keys)
 
 filter_col = header_values[int(dedupe_arg)]
+sort_col = header_values[int(sort_arg)]
+
+# Deduplicating
 
 for key in row_keys:
     row_dict = {}
@@ -32,46 +39,24 @@ for key in row_keys:
         row_dict[header_values[i-1]] = sheet.cell(row=key, column=i).value
     document.append(row_dict)
 
-
-deduped_document = []
-unique_values = []
-
-# for sub in document:
-#     unique_values.append(sub[unique_key])
-# unique_values = list(set(unique_values))
-
-# print('Unique IDs: ', str(unique_values))
-# print('# of IDs: ', len(unique_values))
-
-# deduped_document = [d for d in document if d['ID'] in unique_values]
-
 for d in document:
     if d[filter_col] not in unique_values:
         unique_values.append(d[filter_col])
         deduped_document.append(d)
 
 
-# print('Original length: ', len(document))
+print('Original length: ', len(document))
 print('Deduped length: ', len(deduped_document))
 
 
-def sort_by(e):
-    return e['Admit Term']
+# Sorting
 
-# document.sort(key=sort_by)
+deduped_document.sort(key=sort_by)
+print(f"Sorting by: {sort_col}")
 
-# print('Document: ', document)
+# Writing to new tab
 
-
-# wb.create_sheet(index=1, title='Sorted')
-# sorted_sheet = wb['Sorted']
-# sorted_sheet.append(header_values)
-
-# for i in range(1, len(document)):
-#     sorted_sheet.append(list(document[i].values()))
-sheet_exists = wb['Deduped']
-
-if sheet_exists:
+if 'Deduped' in wb.sheetnames:
     wb.remove(wb['Deduped'])
 
 wb.create_sheet(index=1, title='Deduped')
@@ -83,4 +68,4 @@ for i in range(1, len(deduped_document)):
 
 wb.save(filename=f"{file_arg}.xlsx")
 
-print('Sheetnames: ', wb.sheetnames)
+print("Process Complete!")
